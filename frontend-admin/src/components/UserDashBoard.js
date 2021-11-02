5d2fc5e5684ded1d9fe3e2dcb25a0673218649da
Raghuveer Chimata <chmt.raghuveer@gmail.com>
2021-11-02 11:59:38 +0100

    added individual components for front end
    added individual components for front end

---
+++ frontend-admin/src/components/UserDashBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const UserDashBoard = () =>{
+  return (
+    <div>
+      <h3> This is a initial UserDashBoard</h3>
+      <div>
+      </div>
+    </div>
+  );
+}
+export default UserDashBoard;
---
+++ frontend-vendor/src/components/Sports.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Sports = () =>{
+  return (
+    <div>
+      <h3>Logic of Sports</h3>
+      <div>
+      </div>
+    </div>
+  );
+}
+export default Sports;
